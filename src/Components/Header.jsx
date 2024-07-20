b5c891c283a84f653a277a9700707169e6599028
robsonhansen <robsonrudson65@gmail.com>
2024-07-20 15:19:27 -0300

    feat: Icone de carregar o feed.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
-import Logo from "../Assets/dog.svg";
+import Logo from "../Assets/dog.svg?react";
 import { UserContext } from "../UserContext";
 
 const Header = () => {
@@ -10,8 +10,8 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container}`}>
-        <Link className={styles.logo} to="/login" aria-label="Dogs - Home">
-          <img src={Logo} alt="Dogs" />
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Logo />
         </Link>
         {data
           ? <Link className={styles.login} to="/conta">
